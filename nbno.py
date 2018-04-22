40cb0545d94dd7aa1ca6589bc09942a7308852cb
Lanjelin <lanjelin.svanes@gmail.com>
2018-04-22 19:27:41 +0200

    Støtte for aviser
    Lagt til støtte for aviser

--- nbno.py
+++ nbno.py
@@ -17,6 +17,9 @@ class theURL():
     self.tileWidth = '1024'
     self.tileHeight = '1024'
     self.URL = ''
+    self.URLpart = ''
+    self.URLsep1 = ''
+    self.URLsep2 = ''
   
   def setLevel(self, level):
     self.level = int(level)
@@ -25,13 +28,25 @@ class theURL():
     self.maxLevel = int(maxLevel)
 
   def updateURL(self):
-    self.URL = 'http://www.nb.no/services/image/resolver?url_ver=geneza&urn=URN:NBN:no-nb_digibok_'+\
-    str(self.bokID)+'_'+self.side+\
+    self.URL = self.URLpart+\
+    str(self.bokID)+self.URLsep1+self.side+self.URLsep2+\
     '&maxLevel='+str(self.maxLevel)+'&level='+str(self.level)+\
     '&col='+str(self.col)+'&row='+str(self.row)+\
     '&resX='+str(self.resX)+'&resY='+str(self.resY)+\
     '&tileWidth='+str(self.tileWidth)+'&tileHeight='+str(self.tileWidth)
   
+  def setType(self, bokavis):
+    if (bokavis=='bok'):
+      self.URLpart = 'http://www.nb.no/services/image/resolver?url_ver=geneza&urn=URN:NBN:no-nb_digibok_'
+      self.URLsep1 = '_'
+    elif (bokavis=='avis'):
+      self.URLpart = 'http://www.nb.no/services/image/resolver?url_ver=geneza&urn=URN:NBN:no-nb_digavis_'
+      self.URLsep1 = '-1_'
+      self.URLsep2 = '_null'
+    else:
+      print 'Du mangler å sette type!'
+      exit()
+
   def newURL(self, side, col, row):
     self.side = str(side)
     self.col = int(col)
@@ -77,8 +92,7 @@ def downloadPage(pageNum,bok):
         req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
         response = urllib2.urlopen(req).read()
       except urllib2.HTTPError as e:
-        #print e
-        error = e #spiser bare denne da første ugyldige side spytter ut feilmeldinger før stopp
+        error = e #munch
       else:
         img = Image.open(io.BytesIO(response))
         imageParts.append(img)
@@ -105,14 +119,12 @@ def downloadPage(pageNum,bok):
       r+=1
     newImg.save(folder+str(pageNum)+'.jpg')
     print 'Lagret side '+pageNum+'.jpg'
-    #print 'MaxWidth: '+str(maxWidth)+' MaxHeight: '+str(maxHeight)
-    #print 'SingleWidth: '+str(sinWidth)+' SingleHeight: '+str(sinHeight)
-    #print 'Antall bilder per side: '+str(len(imageParts))
 
 parser = argparse.ArgumentParser()
 optional = parser._action_groups.pop()
 required = parser.add_argument_group('required arguments')
 required.add_argument('--id', metavar='<bokID>', help='IDen på boken som skal lastes ned', default=False)
+optional.add_argument('--avis', action='store_true', help='Settes om det er en avis som lastes', default=False)
 optional.add_argument('--start', metavar='<int>', help='Sidetall å starte på', default=False)
 optional.add_argument('--stop', metavar='<int>', help='Sidetall å stoppe på', default=False)
 optional.add_argument('--level', metavar='<int>', help='Sett Level', default=False)
@@ -122,25 +134,29 @@ args = parser.parse_args()
 
 if args.id:
   x = theURL(str(args.id))
-  print 'Laster ned bok med ID: '+str(args.id)
+  if args.level:
+    x.setLevel(int(args.level))
+  if args.maxlevel:
+    x.setMaxLevel(int(args.maxlevel))
   folder = '.'+os.path.sep+str(x.bokID)+os.path.sep
   try:
     os.stat(folder)
   except:
     os.mkdir(folder)
-  if args.level:
-    x.setLevel(int(args.level))
-  if args.maxlevel:
-    x.setMaxLevel(int(args.maxlevel))
-
-  x.updateMaxColRow('C1')
-  downloadPage('C1',x)
-  x.updateMaxColRow('C2')
-  downloadPage('C2',x)
-  x.updateMaxColRow('C3')
-  downloadPage('C3',x)
-
-  x.updateMaxColRow('0001')
+  if args.avis:
+    x.setType('avis')
+    print 'Laster ned avis med ID: '+str(args.id)
+    x.updateMaxColRow('001')
+  else:  
+    x.setType('bok')
+    print 'Laster ned bok med ID: '+str(args.id)
+    x.updateMaxColRow('C1')
+    downloadPage('C1',x)
+    x.updateMaxColRow('C2')
+    downloadPage('C2',x)
+    x.updateMaxColRow('C3')
+    downloadPage('C3',x)
+    x.updateMaxColRow('0001')
   if args.start:
     pageCounter = int(args.start)
   else:
@@ -150,7 +166,10 @@ if args.id:
   else:
     stopPage = 9999
   while True:
-    downloadPage(str(pageCounter).rjust(4, '0'),x)
+    if args.avis:
+      downloadPage(str(pageCounter).rjust(3, '0'),x)
+    else:  
+      downloadPage(str(pageCounter).rjust(4, '0'),x)
     if (pageCounter == stopPage):
       print 'Ferdig med å laste ned alle sider.'
       exit()
